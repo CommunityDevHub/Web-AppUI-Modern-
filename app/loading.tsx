e0fa4b613b660d2eb83203b6b0f33c3194135655
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-22 17:21:50 +0700

    update:Add->Config-Page-UI

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='w-full flex items-center justify-center text-black'>
+            Loading ..
+        </div>
+    );
+};
+
+export default Loading;
