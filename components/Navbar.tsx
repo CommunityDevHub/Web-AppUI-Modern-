0189525ccfc6a2fda02fd045a5a36c791101d793
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-23 22:19:45 +0700

    update:Add->FixConfigTailwindCss

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '.';
+import { NAV_LINKS } from '@/constants';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <>
+            <nav className='flexBetween max-container padding-container relative z-30 py-5'>
+                
+            </nav>
+        </>
     );
 };
 
