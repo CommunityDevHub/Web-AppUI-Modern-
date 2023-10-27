bad9e198b6004022db8456bf19020b86bc835d28
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-27 22:24:27 +0700

    update:Add->Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,40 @@ const Navbar = () => {
     return (
         <>
             <nav className='flexBetween max-container padding-container relative z-30 py-5'>
-                
+                <Link href="/">
+                    <Image
+                        src="/hilink-logo.svg"
+                        alt='Logo'
+                        width={74}
+                        height={29}
+                    />
+
+                </Link>
+                <ul className='hidden h-full gap-12 lg:flex'>
+                    {NAV_LINKS.map((link) => (
+                        <>
+                            <Link 
+                                href={link.href} 
+                                key={link.key} 
+                                className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:underline'
+                            >
+                                {link.label}
+                            </Link>
+                        </>
+                    ))}
+                </ul>
+
+                <div className='lg:flexCenter hidden'>
+                    {/* Button */}
+                </div>
+
+                <Image
+                    src="menu.svg"
+                    alt="menu"
+                    width={32}
+                    height={32}
+                    className="inline-block cursor-pointer lg:hidden"
+                />
             </nav>
         </>
     );
