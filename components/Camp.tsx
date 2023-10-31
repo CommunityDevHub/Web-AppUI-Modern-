3c04fe4535fe151c20229438ada022f7001fc2b7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-10-31 23:38:04 +0700

    update:Add->Camp

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { PEOPLE_URL } from '@/constants';
+
+interface CampProps {
+    backgroundImage: string;
+    title: string;
+    subtitle: string;
+    peopleJoined: string;
+};
+
+const CampSite = () => {
+    return (
+        <>
+
+        </>
+    );
+};
 
 const Camp = () => {
     return (
-        <div>Camp</div>
+        <>
+            
+        </>
     );
 };
 
