afedad27c143bba252c65c33aaa6e03afdfca0be
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-01 23:46:11 +0700

    update:Add->Guide

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Guide = () => {
     return (
-        <div>Guide</div>
+        <>
+            <section className='flexCenter flex-col'>
+                <div className='padding-container max-container w-full pb-24'>
+                    <Image
+                        src="/camp.svg"
+                        alt='Camp'
+                        width={50}
+                        height={50}
+                    />
+                </div>
+            </section>
+        </>
     );
 };
 
