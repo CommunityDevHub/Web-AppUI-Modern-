8d602c84f89877612591caf511e7611024819108
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-13 22:10:46 +0700

    update:Add->HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Suspense, useEffect, useRef, useState } from 'react';
+
+import sakura from "../assets/sakura.mp3";
+import { HomeInfo, Loader } from '../components';
+import { soundoff, soundon } from '../assets/icons';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <section className='w-full h-screen relative'>
+                <div 
+                    className='absolute top-28 left-0 right-0 z-10 
+                    flex items-center justify-center'
+                >
+
+                </div>
+            </section>
+        </>
     );
 };
 
