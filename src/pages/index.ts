034c698d82f196a4079b8f78659d72c99c6c4dd6
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-13 21:13:04 +0700

    update:Add->LinkPage

---
+++ src/pages/index.ts
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Project from "./Project";
+
+export {
+    Home, 
+    About,
+    Contact,
+    Project,
+};
