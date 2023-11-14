567aacbd4644aaa3da01cda1a99b7e3f903ebb1b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-14 22:06:53 +0700

    update:Add->FixStyleAbout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -59,6 +59,56 @@ const About = () => {
                             Build some mini projects, leveling up my skills everyday and do paper research about Detection Object
                         </p>
                     </div>
+
+                    <div className='mt-12 flex'>
+                        <VerticalTimeline>
+                            {experiences.map((experience) => (
+                                <VerticalTimelineElement
+                                    key={experience.company_name}
+                                    date={experience.date}
+                                    iconStyle={{ background: experience.iconBg }}
+                                    icon={
+                                        <div className='flex justify-center items-center w-full h-full cursor-pointer border-4 rounded-full'>
+                                            <img
+                                                src={experience.icon}
+                                                alt={experience.company_name}
+                                                className='w-[60%] h-[60%] object-contain'
+                                            />
+                                        </div>
+                                    }
+                                    contentStyle={{
+                                        borderBottom: "8px",
+                                        borderStyle: "solid",
+                                        borderBottomColor: experience.iconBg,
+                                        boxShadow: "none",
+                                    }}
+                                >
+                                    <div className="border-2 p-4 rounded-xl">
+                                        <h3 className='text-black text-xl font-poppins font-semibold'>
+                                            {experience.title}
+                                        </h3>
+                                        <p
+                                            className='text-black-500 font-medium text-base'
+                                            style={{ margin: 0 }}
+                                        >
+                                            {experience.company_name}
+                                        </p>
+                                    </div>
+
+                                    <ul className='my-5 list-disc ml-5 space-y-2'>
+                                        {experience.points.map((point, index) => (
+                                            <li
+                                                key={`experience-point-${index}`}
+                                                className='text-black-500/50 font-normal pl-1 text-sm'
+                                            >
+                                            {point}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </VerticalTimelineElement>
+                            ))}
+                        </VerticalTimeline>
+                    </div>
                 </div>
             </section>
         </>
